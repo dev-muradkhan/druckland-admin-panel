48a5f58abb60a3fdc29d0d183a1d5066883c25a9
Md Murad Khan <dev.muradkhan@gmail.com>
2025-04-07 15:26:05 +0600

    This branch for developing stage before marge with Main Branch

--- src/components/users/EditUserPage.tsx
+++ src/components/users/EditUserPage.tsx
@@ -10,7 +10,7 @@ import { User, UserFormData } from '@/types/users';
 interface EditUserPageProps {
   initialUser: User; // Receive initial user data as prop
   userId: string;     // Receive user ID as prop
-  // Receive user ID as prop
+
 }
 
 export default function EditUserPage({ initialUser, userId }: EditUserPageProps) { 
